c1b04183ae853b09ae6e12f4d5860b6baf44830a
nima <nmoosavi@live.com>
2021-05-16 14:29:27 +0430

    change kafka structure and create Crud General

--- src/main/java/com/nicico/cost/crud/repository/GeneralRepository.java
+++ src/main/java/com/nicico/cost/crud/repository/GeneralRepository.java
@@ -6,21 +6,67 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @param <T> is the entity class that you must Extended to BaseEntity class {@link com.nicico.cost.crud.domain.entity.BaseEntity}
+ * @param <I> is the type of data base Identity class such as Long,String, ...
+ * @author nima
+ * @version 1.0.1
+ * @implNote this class used for connect to data Base And you must implement this service in Your Library
+ */
 public interface GeneralRepository<T extends BaseEntity<I>, I extends Serializable> {
+    /**
+     * @param t the Entity View Model that you must Add To Data Base
+     * @return the Optional Of Entity that save it in data base
+     * @apiNote this method used for save Data in Data base
+     */
     Optional<T> save(T t);
 
+    /**
+     * @param id the incrementalId of data base Object
+     * @param t  the Entity View Model that you must Add To Data Base
+     * @return the Optional Of Entity that save it in data base
+     * @apiNote this method used for Update data Base Object
+     */
     Optional<T> update(I id, T t);
 
+    /**
+     * @param tList the list of Entity that you must save it in Data base
+     * @return the Optional List Of Entity and their Ids
+     * @apiNote used for save the batch of Data in Data base
+     */
     Optional<List<T>> saveAll(List<T> tList);
 
+    /**
+     * @param id the incrementalId of data base Object
+     * @return the Optional Of Entity that save it in data base
+     * @apiNote used for fetch Data By IncrementalId
+     */
     Optional<T> findById(I id);
 
+    /**
+     * @return the List Of Entities
+     * @apiNote this methode uses for Fetch All Data
+     */
     Optional<List<T>> findAll();
 
+    /**
+     * @param page     the page number that you must fetch it
+     * @param pageSize the page Size of that you need to split Data
+     * @return the Optional List Of Entity from Response Of Data Base
+     */
     Optional<List<T>> findAll(int page, int pageSize);
 
+    /**
+     * @return the Number Of data
+     * @apiNote method used for get the count Of Data
+     */
     int count();
 
+    /**
+     * @param id is the incrementalId of Object that you need too remove it from Data Base
+     * @return the true or false Of result
+     * @apiNote Used for delete Object From Incremental Id
+     */
     Optional<Boolean> deleteById(I id);
 
 
